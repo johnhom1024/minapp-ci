938949b9c87291afe3da6314534cf66c610e8a44
anthony <924358746@qq.com>
2022-07-29 22:39:26 +0800

    feat: 完善命令行调用上传函数

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { program } from 'commander';
+import MpUpload from './util/index';
 
+program
+  .command('upload')
+  .description('上传当前小程序代码')
+  .action(() => {
+    MpUpload();
+  })
 
-program.command('upload').description('上传当前小程序代码')
+program
+  .version('1.0.0', '-v --version')
+  .usage('miniapp-cli <command> [option]');
+
+program.parse(process.argv);
